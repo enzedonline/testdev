1e3162113ae0ad7a170966eef98b4c6604347918
Richard Allen <richard@enzedonline.com>
2023-02-20 10:47:35 +0100

    rich text to plain basic filter and tag

--- home/templatetags/home_tags.py
+++ home/templatetags/home_tags.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 from django import template
 from wagtail.templatetags.wagtailcore_tags import richtext
+from html import unescape
+from django.template.defaultfilters import linebreaksbr
+
 register = template.Library()
 
 @register.filter
@@ -24,4 +27,23 @@ def rich_text_parser(element):
                         element.decompose()
                 except AttributeError:
                     element.decompose()
-    
+
+NON_BREAKING_ELEMENTS = ['a', 'abbr', 'acronym', 'audio', 'b', 'bdi', 'bdo', 'big', 'button', 
+    'canvas', 'cite', 'code', 'data', 'datalist', 'del', 'dfn', 'em', 'embed', 'i', 'iframe', 
+    'img', 'input', 'ins', 'kbd', 'label', 'map', 'mark', 'meter', 'noscript', 'object', 'output', 
+    'picture', 'progress', 'q', 'ruby', 's', 'samp', 'script', 'select', 'slot', 'small', 'span', 
+    'strong', 'sub', 'sup', 'svg', 'template', 'textarea', 'time', 'u', 'tt', 'var', 'video', 'wbr']
+
+@register.filter()
+def plaintext(richtext):
+    return BeautifulSoup(unescape(richtext), "html.parser").get_text(separator=" ")
+
+@register.simple_tag()
+def html_to_text(markup, preserve_new_lines=True, strip_tags=['style', 'script', 'code']):
+    soup = BeautifulSoup(unescape(markup), "html.parser")
+    for element in soup(strip_tags): element.extract()
+    if preserve_new_lines:
+        for element in soup.find_all():
+            if element.name not in NON_BREAKING_ELEMENTS:
+                element.append('\n') if element.name == 'br' else element.append('\n\n')
+    return linebreaksbr(soup.get_text(" "))
