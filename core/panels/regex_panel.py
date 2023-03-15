1323bfdfb2ad5b360161baf5b77ca85b3026a06c
Richard Allen <richard@enzedonline.com>
2023-03-15 13:28:57 +0100

    docstring

--- core/panels/regex_panel.py
+++ core/panels/regex_panel.py
@@ -5,7 +5,15 @@ from django.utils.safestring import mark_safe
 from django.forms.fields import SlugField
 from django.core.exceptions import ImproperlyConfigured
 
+
 class RegexPanel(FieldPanel):
+    """
+    FieldPanel that ignores keys not within regex pattern.
+    Usage: RegexPanel('field', 'pattern').
+    If field inherits Slugfield and pattern ommitted, default slug filter "^[-\w]+$" applied.
+    Feedback restricted to flashing input text to warning colour. 
+    Recommended to supply valid help string and aria feedback if needed.
+    """
     def __init__(self, field_name, pattern=None, *args, **kwargs):
         self.pattern = pattern
         super().__init__(field_name, *args, **kwargs)
@@ -25,19 +33,21 @@ class RegexPanel(FieldPanel):
                     "pattern", self.panel.pattern
                 )
             elif issubclass(self.form.fields[self.field_name].__class__, SlugField):
-                self.form.fields[self.field_name].__setattr__(
-                    "pattern", '^[-\w]+$'
-                )
+                self.form.fields[self.field_name].__setattr__("pattern", "^[-\w]+$")
             else:
-                raise ImproperlyConfigured(_("Field must be a SlugField or have a pattern set in the panel definition."))
+                raise ImproperlyConfigured(
+                    _(
+                        "Field must inherit SlugField or have a pattern set in the panel definition."
+                    )
+                )
 
         def render_html(self, parent_context) -> str:
             html = super().render_html(parent_context)
-            soup = BeautifulSoup(html, 'html.parser')
-            input_element = soup.find('input')
+            soup = BeautifulSoup(html, "html.parser")
+            input_element = soup.find("input")
             if input_element:
-                input_element['onkeydown']="return regex_keydownhandler(event)"
-                script = soup.new_tag('script')
+                input_element["onkeydown"] = "return regex_keydownhandler(event)"
+                script = soup.new_tag("script")
                 script.string = f'\
                     const inputElement = document.getElementById("{input_element["id"]}"); \
                     const textColour = window.getComputedStyle(inputElement).color; \
@@ -52,4 +62,3 @@ class RegexPanel(FieldPanel):
                     }}'
                 soup.append(script)
             return mark_safe(str(soup.prettify()))
-
