b93cf0197c5982e01fc814518f7563d2571760e4
Richard Allen <richard@enzedonline.com>
2023-03-17 10:51:44 +0100

    +(file filter, fieldname, responsive textarea resize)

--- core/panels/file_to_text_field_panel.py
+++ core/panels/file_to_text_field_panel.py
@@ -4,27 +4,63 @@ from django.utils.translation import gettext_lazy as _
 
 
 class FileToTextFieldPanel(FieldPanel):
-    class BoundPanel(FieldPanel.BoundPanel):       
-        def render_html(self, parent_context = None):
+    def __init__(self, field_name, file_type_filter=None, *args, **kwargs):
+        self.file_type_filter = file_type_filter
+        super().__init__(field_name, *args, **kwargs)
+
+    def clone_kwargs(self):
+        kwargs = super().clone_kwargs()
+        kwargs.update(
+            file_type_filter=self.file_type_filter,
+        )
+        return kwargs
+
+    class BoundPanel(FieldPanel.BoundPanel):
+        def __init__(self, **kwargs):
+            super().__init__(**kwargs)
+            self.accepts = (
+                f'accept="{self.panel.file_type_filter}" ' if self.panel.file_type_filter else ""
+            )
+
+
+        msg = {
+            'file_label': _("Read data from file"),
+        }
+
+        def render_html(self, parent_context=None):
             html = super().render_html(parent_context)
             return mark_safe(html + self.file_to_text_field_button())
 
         def file_to_text_field_button(self):
+            file_input_id = f'{self.field_name}File'
             return '''
-                <label for="''' + self.field_name + '''File"> 
-                    <span class="w-panel__heading w-panel__heading--label">''' + _("Read data from file") + '''</span> 
+                <label for="''' + file_input_id + '''"> 
+                    <h4 class="w-panel__heading w-panel__heading--label svg-panel-label">
+                        ''' + self.msg['file_label'] + '''
+                    </h4> 
                 </label> 
-                <input type="file" id="''' + self.field_name + '''File" style="border-style: none; padding: 0;" />
-                <script> 
-                    const ''' + self.field_name + '''File = document.getElementById("''' + self.field_name + '''File"); 
-                    ''' + self.field_name + '''File.addEventListener("change", (e) => {
+                <input 
+                    type="file" 
+                    id="''' + file_input_id + '''" 
+                    ''' + self.accepts + ''' 
+                    style="border-style: none; padding: 0; display: block; width: fit-content;" 
+                />
+                <script>
+                    input''' + file_input_id + ''' = document.getElementById("''' + file_input_id + '''");
+                    textField''' + self.id_for_label() + ''' = document.getElementById("''' + self.id_for_label() + '''");
+                    textField''' + self.id_for_label() + '''.style.maxHeight='20em';
+                    textField''' + self.id_for_label() + '''.style.overflowY='auto';
+                    input''' + file_input_id + '''.addEventListener("change", (e) => {
                         e.preventDefault(); 
-                        const input = ''' + self.field_name + '''File.files[0]; 
+                        const input = input''' + file_input_id + '''.files[0]; 
                         const reader = new FileReader(); 
                         reader.onload = function (e) {
-                            const svgField = document.getElementById("''' + self.id_for_label() + '''"); 
-                            svgField.value = e.target.result; 
+                            textField''' + self.id_for_label() + '''.value = e.target.result;
+                            textField''' + self.id_for_label() + '''.style.height=0;
+                            textField''' + self.id_for_label() + '''.style.height=textField 
+                            ''' + self.id_for_label() + '''.scrollHeight + 5 + 'px';
                         }; 
                         reader.readAsText(input); 
-                    }); 
-                </script>'''    
+                    });                
+                </script>
+                '''    
