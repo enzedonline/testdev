a37c23f0e15f384f563acd439a987c39f8393977
Richard Allen <richard@enzedonline.com>
2023-06-07 13:26:33 +1200

    Fix due to changes in 5.x title/slug sync

--- blog/categories.py
+++ blog/categories.py
@@ -3,7 +3,24 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from wagtail.admin.panels import FieldPanel
 from wagtail.snippets.models import register_snippet
+from wagtail.admin.widgets.slug import SlugInput
+from wagtail.admin.forms import WagtailAdminModelForm
 
+
+# <input id="id_title" type="text" name="title" maxlength="100" required="">
+# <input id="id_title" type="text" name="title"
+#     data-controller="w-sync"
+#     data-action="focus->w-sync#check blur->w-sync#apply change->w-sync#apply"
+#     data-w-sync-target-value="body:not(.page-is-live) #some_other_slug"
+# />
+
+class CategoryForm(WagtailAdminModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs['data-controller'] = 'w-sync'
+        self.fields['title'].widget.attrs['data-action'] = 'focus->w-sync#check blur->w-sync#apply change->w-sync#apply'
+        self.fields['title'].widget.attrs['data-w-sync-target-value'] = '#id_slug'
+        
 @register_snippet
 class BlogCategory(models.Model):
     title = models.CharField(max_length=100, verbose_name=_("Category Name"))
@@ -14,9 +31,11 @@ class BlogCategory(models.Model):
         allow_unicode=True,
     )
 
+    base_form_class = CategoryForm
+
     panels = [
         FieldPanel("title", classname="title"),
-        FieldPanel("slug"),
+        FieldPanel("slug", widget=SlugInput),
     ]
 
     class Meta:
