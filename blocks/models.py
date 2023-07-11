065b9118eb15670881a13e67bf773cac1087a706
Richard Allen <richard@enzedonline.com>
2023-07-11 15:34:57 +1200

    +CollapsibleCardBlock

--- blocks/models.py
+++ blocks/models.py
@@ -2,3 +2,4 @@ from .heading import HeadingBlock
 from .csv_table import CSVTableBlock
 from .parsed_richtext import ParsedRichTextBlock
 from .import_text import ImportTextBlock
+from .collapsible_card import CollapsibleCardBlock
