1e3660b56fa440f841781561957376dc7b7176f7
Richard Allen <richard@enzedonline.com>
2023-08-10 15:33:44 +1200

    use preview_name instead of hard coded 'preview'

--- core/widgets/snippet_preview_chooser.py
+++ core/widgets/snippet_preview_chooser.py
@@ -17,28 +17,35 @@ class PreviewMixin:
     @mark_safe
     def get_preview(self, instance):
         """
-        Snippet model should have a 'preview' property that returns 
+        Snippet model should have a 'preview' property that returns
         - a wagtail image (or custom subclass)
         - a rendered html string for the preview image for that instance
         - a registered icon name
         If preview is None or does not exist, defaults to viewset/model icon if exists, else the default snippet icon
         """
-        def render_icon(icon=None):
-            if not icon: icon = getattr(self, "icon", getattr(instance, "get_icon", "snippet"))
-            return render_to_string('wagtailadmin/shared/icon.html', context=get_icon(icon))
 
-        preview = getattr(instance, "preview", None)
+        def render_icon(icon=None):
+            if not icon:
+                icon = getattr(self, "icon", getattr(instance, "get_icon", "snippet"))
+            return render_to_string(
+                "wagtailadmin/shared/icon.html", context=get_icon(icon)
+            )
+
+        preview = getattr(instance, self.preview_name, None)
         if preview:
             if isinstance(preview, Image):
                 return preview.get_rendition("height-60").img_tag()
             else:
-                if is_html(preview):# preview is html formatted string
+                if is_html(preview):  # preview is html formatted string
                     return preview
-                else:               # if preview is slug, assume preview is icon name else render viewset/model/default icon
-                    return render_icon(preview) if slugify(preview) == preview else render_icon()
+                else:  # if preview is slug, assume preview is icon name else render viewset/model/default icon
+                    return (
+                        render_icon(preview)
+                        if slugify(preview) == preview
+                        else render_icon()
+                    )
         else:
-            return render_icon()    # no preview, render icon instead
-    
+            return render_icon()  # no preview, render icon instead
 
 
 class SnippetPreviewChooser(PreviewMixin, AdminSnippetChooser):
@@ -46,13 +53,15 @@ class SnippetPreviewChooser(PreviewMixin, AdminSnippetChooser):
     Extends the default snippet chooser to include a dynamic preview icon.
     Add preview method to snippet class to return image, icon name or rendered image HTML
     """
+
     template_name = "widgets/snippet_preview_chooser.html"
     js_constructor = "SnippetPreviewChooser"
     classname = "snippet-preview-chooser"
 
-    def __init__(self, model, url_prefix=None, **kwargs):
+    def __init__(self, model, url_prefix=None, preview_name="preview", **kwargs):
         super().__init__(model, **kwargs)
         self.preview_key = "preview"
+        self.preview_name = preview_name
         self.url_prefix = url_prefix if url_prefix else model._meta.model_name
         if not self.icon:
             self.icon = getattr(model, "icon", "snippet")
@@ -87,6 +96,7 @@ class SnippetPreviewChooser(PreviewMixin, AdminSnippetChooser):
 
 class SnippetPreviewChooserAdapter(SnippetChooserAdapter):
     """Register SnippetPreviewChooser for use in blocks"""
+
     js_constructor = "core.widgets.choosers.SnippetPreviewChooser"
 
     @cached_property
@@ -97,10 +107,14 @@ class SnippetPreviewChooserAdapter(SnippetChooserAdapter):
             ]
         )
 
+
 register(SnippetPreviewChooserAdapter(), SnippetPreviewChooser)
 
 
 class SnippetPreviewChosenView(PreviewMixin, ChosenView):
+    # model fieldname or property used to return preview image
+    preview_name = "preview"
+
     # add preview to returned chosen data
     def get_chosen_response(self, item):
         return self._wrap_chosen_response_data(
@@ -121,8 +135,10 @@ class SnippetPreviewChooserViewSet(ChooserViewSet):
             model = Product
         product_chooser_viewset = ProductChooserViewSet()
     """
+
     def __init__(self, name=None, *args, **kwargs):
-        if not name: name = self.model._meta.model_name
+        if not name:
+            name = self.model._meta.model_name
         super().__init__(name=name, *args, **kwargs)
 
     chosen_view_class = SnippetPreviewChosenView
@@ -133,7 +149,11 @@ class SnippetPreviewChooserViewSet(ChooserViewSet):
 
     @cached_property
     def widget_class(self):
-        return SnippetPreviewChooser(model=self.model, url_prefix=self.name)
+        return SnippetPreviewChooser(
+            model=self.model,
+            url_prefix=self.name,
+            preview_name=SnippetPreviewChosenView.preview_name,
+        )
 
     class Meta:
         abstract = True
