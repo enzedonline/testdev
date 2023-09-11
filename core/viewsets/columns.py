12e151b8fd537aa58b10f2e8c04c11d469031057
Richard Allen <richard@enzedonline.com>
2023-09-11 14:11:39 +1200

    ImageColumn -custom template -> +format value

--- core/viewsets/columns.py
+++ core/viewsets/columns.py
@@ -1,4 +1,12 @@
 from wagtail.admin.ui.tables import TitleColumn
+from django.utils.safestring import mark_safe
 
 class ImageColumn(TitleColumn):
-    cell_template_name = "viewsets/image_cell.html"
+    def get_cell_context_data(self, instance, parent_context):
+        context = super().get_cell_context_data(instance, parent_context)
+        try:
+            context['value'] = mark_safe(context['value'].get_rendition('height-50').img_tag())
+        except:
+            pass
+        return context
+
