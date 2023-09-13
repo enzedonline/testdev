a0c1897b75e4d05515cecb98acf7f46068604c91
Richard Allen <richard@enzedonline.com>
2023-09-13 15:10:25 +1200

    fix imports

--- blocks/wagtail/__init__.py
+++ blocks/wagtail/__init__.py
@@ -1,1 +1,5 @@
-from .blocks import *
+from .blocks import ImageChooserBlock
+from .blocks import CharBlock
+from .blocks import ChoiceBlock
+from .blocks import TextBlock
+from .blocks import URLBlock
