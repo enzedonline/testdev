f234de0ed31f2fe7cace39176d053ba43a2ba0c2
Richard Allen <richard@enzedonline.com>
2023-09-20 18:19:25 +1200

    Make breakpoint attribute of menu model

--- menu/models.py
+++ menu/models.py
@@ -2,18 +2,19 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import StreamField
-from wagtail.models import (
-    PreviewableMixin,
-    DraftStateMixin,
-    RevisionMixin,
-    LockableMixin,
-    WorkflowMixin,
-    TranslatableMixin,
-)
+from wagtail.models import (DraftStateMixin, LockableMixin, PreviewableMixin,
+                            RevisionMixin, TranslatableMixin, WorkflowMixin)
 from wagtail.snippets.models import register_snippet
 
 from .blocks import MenuStreamBlock
 
+BREAKPOINT_CHOICES = (
+    # ('none', _("No breakpoint (always collapsed)")),
+    # ('sm', _("Mobile screens only (<576px)")),
+    ('md', _("Small Screens (<768px)")),
+    ('lg', _("Medium Screens (<992px)")),
+    # ('xl', _("Extra Large (<1200px)")),
+)
 
 @register_snippet
 class Menu(
@@ -38,11 +39,20 @@ class Menu(
     items = StreamField(
         MenuStreamBlock(), verbose_name="Menu Items", blank=True, use_json_field=True
     )
+    breakpoint = models.CharField(
+        max_length=4,
+        choices=BREAKPOINT_CHOICES,
+        default='lg', 
+        blank=False, 
+        null=True,  
+        verbose_name=_("Mobile Layout Breakpoint")
+    )
 
     panels = [
         FieldPanel("title"),
         FieldPanel("slug"),
         FieldPanel("logo"),
+        FieldPanel("breakpoint"),
         FieldPanel("items"),
     ]
 
