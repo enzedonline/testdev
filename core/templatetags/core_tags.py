3df91493c1957cf7902d07631e43ee2d9a8d3918
Richard Allen <richard@enzedonline.com>
2024-01-27 17:02:27 +1300

    +doc_title_to_url

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -1,6 +1,8 @@
-from django import template
 import secrets
 import string
+
+from django import template
+from wagtail.documents.models import Document
 
 register = template.Library()
 
@@ -8,3 +10,12 @@ register = template.Library()
 def random_string(length=8):
     characters = string.ascii_letters + string.digits
     return ''.join(secrets.choice(characters) for _ in range(length))
+
+@register.filter()
+def doc_title_to_url(title):
+    try:
+        document = Document.objects.get(title=title)
+        return document.url
+    except:
+        pass
+    
