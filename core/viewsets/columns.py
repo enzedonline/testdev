ff45a87cccac5ccb1c83ea621e61b223b959f5e1
Richard Allen <richard@enzedonline.com>
2024-02-03 11:54:49 +1300

    +show-transparency, render image icon if value not image

--- core/viewsets/columns.py
+++ core/viewsets/columns.py
@@ -5,8 +5,12 @@ class ImageColumn(TitleColumn):
     def get_cell_context_data(self, instance, parent_context):
         context = super().get_cell_context_data(instance, parent_context)
         try:
-            context['value'] = mark_safe(context['value'].get_rendition('height-50').img_tag())
+            context['value'] = mark_safe(
+                context['value'].get_rendition('height-50').img_tag({'class':"show-transparency"})
+                )
         except:
-            pass
+            context['value'] = mark_safe(
+                '<svg class="icon icon-image" height="50px" viewBox="0 0 24 24" aria-hidden="true"><use href="#icon-image"></use></svg>'
+                )
         return context
 
