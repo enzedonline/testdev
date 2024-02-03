23e6d7ca896396d3f111e69b8a4a38aa0743396a
Richard Allen <richard@enzedonline.com>
2024-02-04 11:07:29 +1300

    +TemplateText, +TemplateTextSetItem

--- site_settings/models.py
+++ site_settings/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
-from wagtail.admin.panels import FieldPanel, InlinePanel, PageChooserPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.contrib.settings.models import BaseSiteSetting, register_setting
 from wagtail.models import Orderable
 from wagtail.snippets.models import register_snippet
@@ -40,4 +40,59 @@ class Tokens(BaseSiteSetting):
         verbose_name=_("OpenAI Key")
     )    
 
+@register_snippet
+class TemplateText(ClusterableModel):
+    template_set = models.CharField(
+        unique=True,
+        max_length=50,
+        verbose_name="Set Name",
+        help_text=_("The set needs to be loaded in template tags then text references as {{set.tag}}")
+    )    
 
+    panels = [
+        FieldPanel("template_set"),
+        MultiFieldPanel(
+            [
+                InlinePanel("templatetext_items"),
+            ],
+            heading=_("Text Items"),
+        ),
+    ]
+
+    def __str__(self):
+        return self.template_set
+    
+    class Meta:
+        verbose_name = _('Template Text')
+
+class TemplateTextSetItem(Orderable):
+    set = ParentalKey(
+        "TemplateText",
+        related_name="templatetext_items",
+        help_text=_("Template Set to which this item belongs."),
+        verbose_name="Set Name",
+    )
+    template_tag = models.CharField(
+        max_length=50,
+        help_text=_("Enter a tag without spaces, consisting of lowercase letters, numbers, and underscores.\nThe first character must be a lowercase letter."),
+        verbose_name="Template Tag",
+    )    
+    text = models.TextField(
+        null=True,
+        blank=True,
+        help_text=_("The text to be inserted in the template.")
+    )
+
+    panels = [
+        FieldPanel('template_tag'),
+        FieldPanel('text'),
+    ]
+
+    def __str__(self):
+        return self.template_tag
+
+    class Meta:
+        unique_together = ('set', 'template_tag')
+        # constraints = [
+        #     models.UniqueConstraint(fields=['set', 'template_tag'], name='unique_set_template_tag')
+        # ]
