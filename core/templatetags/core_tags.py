bafafd555882a85484797d8432d640300e8a5ced
Richard Allen <richard@enzedonline.com>
2024-02-11 14:48:35 +1300

    +renditions

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -3,6 +3,7 @@ import string
 
 from django import template
 from wagtail.documents.models import Document
+from wagtail.templatetags.wagtailcore_tags import richtext
 
 register = template.Library()
 
@@ -18,4 +19,21 @@ def doc_title_to_url(title):
         return document.url
     except:
         pass
-    
+
+@register.filter()
+def clean_rich_text(rich_text):
+    render = richtext(rich_text)
+    return render
+
+@register.simple_tag()
+def get_rendition(image, image_options):
+    return image.get_rendition(image_options)
+
+@register.simple_tag()
+def get_picture_rendition(image, width):
+    file = getattr(image, 'file', False)
+    if file:
+        if width < image.file.width:
+            return image.get_rendition(f"width-{width}|format-webp")
+        else:
+            return image.get_rendition("original|format-webp")
