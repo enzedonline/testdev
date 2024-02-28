e4326fa49e63c6ca244a1a548bc08f806318a37b
Richard Allen <richard@enzedonline.com>
2024-02-29 12:44:35 +1300

    Drop RequiredMixin, declare child blocks in init to inherit required

--- blocks/seo_image_chooser.py
+++ blocks/seo_image_chooser.py
@@ -1,75 +1,60 @@
+from django.forms.utils import ErrorList
 from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
-from wagtail.blocks import StructBlock
-from wagtail.blocks.struct_block import StructBlockAdapter
+from wagtail.blocks import CharBlock, StructBlock
+from wagtail.blocks.struct_block import (StructBlockAdapter,
+                                         StructBlockValidationError)
 from wagtail.telepath import register
 
-from blocks.wagtail.blocks import CharBlock, ImageChooserBlock, RequiredMixin
+from blocks.base_blocks import CompactImageChooserBlock
 
 
-class SEOImageChooserBlock(RequiredMixin, StructBlock):
+class SEOImageChooserBlock(StructBlock):
+    def __init__(self, required=True, **kwargs):
+        local_blocks = (
+            ("image", CompactImageChooserBlock(
+                label=_("Image"),
+                required=required
+            )),
+            ("description", CharBlock(
+                label=_("Description"),
+                help_text=_(
+                    "A contextual description of the image for screen readers and search engines"
+                ),
+                required=required
+            )),
+        )   
+        super().__init__(local_blocks, **kwargs)
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        if kwargs:
-            self.declared_blocks["image"].required = kwargs.get("required", True)
-            self.declared_blocks["seo_title"].required = kwargs.get("required", True)
-
-    image = ImageChooserBlock(label=_("Image"))
-    seo_title = CharBlock(
-        label=_("SEO Title"),
-        help_text=_(
-            "A text description of the image for screen readers and search engines"
-        ),
-    )
+    
 
     class Meta:
+        icon = "image"
+        label_format = '{seo_title}'
         template = 'blocks/image_block.html'
-        form_classname = "seo-image-chooser-block"
-
-    @cached_property
-    def image_error(self):
-        from django.forms.utils import ErrorList
-        return ErrorList([_("Please select an image")])
-
-    @cached_property
-    def title_error(self):
-        from django.forms.utils import ErrorList
-        return ErrorList([_("Please enter a descriptive SEO title for the image")])
+        form_classname = "structblock seo-image-chooser-block"
 
     def clean(self, value):
-        errors = {}
-        image = value.get("image")
-        seo_title = value.get("seo_title")
-        if self.required:
-            if not image:
-                errors["image"] = self.image_error
-            if not seo_title:
-                errors["seo_title"] = self.title_error
-        elif image and not seo_title:
-            errors["seo_title"] = self.title_error
-
-        if errors:
-            from wagtail.blocks import StructBlockValidationError
-            raise StructBlockValidationError(block_errors=errors)
-
-        return super().clean(value)
-
+        # standard form validation
+        cleaned_data = super().clean(value)
+        # custom validation to run if standard form validation passes
+        if not self.required and (bool(value['image']) and not bool(value['description'])):
+            raise StructBlockValidationError(
+                block_errors={'description': ErrorList(
+                    [_("Please enter a text description for the image.")])}
+            )
+        return cleaned_data
 
 class SEOImageChooserBlockAdapter(StructBlockAdapter):
-    from django.utils.functional import cached_property
-
-    js_constructor = "blocks.models.SEOImageChooserBlock"
+    js_constructor = "blocks.seo_image_chooser.SEOImageChooserBlock"
 
     @cached_property
     def media(self):
         from django import forms
-
         structblock_media = super().media
         return forms.Media(
             js=structblock_media._js + ["js/seo-image-chooser-block.js"],
             css={"all": ("css/seo-image-chooser-block.css",)},
         )
-
-
+    
 register(SEOImageChooserBlockAdapter(), SEOImageChooserBlock)
--- blocks/static/js/seo-image-chooser-block.js
+++ blocks/static/js/seo-image-chooser-block.js
@@ -1,33 +1,33 @@
-// js/seo-image-block.js
+// js/seo-image-chooser-block.js
 
 class SEOImageChooserBlockDefinition extends window.wagtailStreamField.blocks
     .StructBlockDefinition {
+    // hide description field if required=False and no image selected
+    // apply required mark to description as will always be required if shown
+
     render(placeholder, prefix, initialState, initialError) {
+        // set description field required=true - only affects rendering
+        this.childBlockDefs.find(obj => obj.name === 'description').meta.required = true;
+
         const block = super.render(
             placeholder,
             prefix,
             initialState,
             initialError,
         );
-        const structBlock = block.container[0];
-        const imageInput = structBlock.querySelector(`#${prefix}-image`);
-        const seoTitleSection = structBlock.querySelector('[data-contentpath="seo_title"]');
-        const seoTitleLabel = seoTitleSection.querySelector("label.w-field__label");
-
-        const requiredMark = document.createElement('span');
-        requiredMark.innerText = "*";
-        requiredMark.className = "w-required-mark";
-        seoTitleLabel.appendChild(requiredMark)
-
-        const updateDisplay = () => {
-            seoTitleSection.style.display = imageInput.value.trim() === "" ? "none" : "block";
-        };
-
-        imageInput.addEventListener("change", updateDisplay);
-        updateDisplay();
-
+        if (!this.required) {
+            // StructBlock is optional, hide description section if no image value
+            const structBlock = block.container[0];
+            const imageInput = structBlock.querySelector(`#${prefix}-image`);
+            const descriptionSection = structBlock.querySelector('[data-contentpath="description"]');
+            const updateDisplay = () => {
+                descriptionSection.style.display = imageInput.value.trim() === "" ? "none" : "block";
+            };
+            imageInput.addEventListener("change", updateDisplay);
+            updateDisplay();
+        }
         return block;
     }
 }
 
-window.telepath.register('blocks.models.SEOImageChooserBlock', SEOImageChooserBlockDefinition);
+window.telepath.register('blocks.seo_image_chooser.SEOImageChooserBlock', SEOImageChooserBlockDefinition);
