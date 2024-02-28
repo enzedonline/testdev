48ecb2ae250c0e615a5d9cf9661f01f0ab3bba1f
Richard Allen <richard@enzedonline.com>
2024-02-29 12:48:33 +1300

    sorting

--- blocks/models.py
+++ blocks/models.py
@@ -1,5 +1,7 @@
+from .code import BaseCodeBlock
 from .collapsible_card import CollapsibleCardBlock
 from .csv_table import CSVTableBlock
+from .django_html import DjangoTemplateFragmentBlock
 from .external_link import ExternalLinkEmbedBlock
 from .flex_card import FlexCardBlock
 from .heading import HeadingBlock
@@ -7,6 +9,6 @@ from .import_text import ImportTextBlock
 from .links import LinkBlock
 from .parsed_richtext import ParsedRichTextBlock
 from .product import ProductBlock
-from .rich_text import RichTextStructBlock, MinimalRichTextBlock, SimpleRichTextBlock, BasicRichTextBlock
+from .rich_text import (BasicRichTextBlock, MinimalRichTextBlock,
+                        RichTextStructBlock, SimpleRichTextBlock)
 from .seo_image_chooser import SEOImageChooserBlock
-from .django_html import DjangoTemplateFragmentBlock
