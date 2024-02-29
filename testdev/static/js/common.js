a036b1ab9c47e7bd45e6d98aed67644b5ca02664
Richard Allen <richard@enzedonline.com>
2024-02-29 13:07:21 +1300

    include_js > promise that waits for script load, +highlightCodeBlock, +copyToClipboard

---
+++ testdev/static/js/common.js
@@ -0,0 +1,120 @@
+let localDateTime = (element, date) => {
+  // Only run if element exists and date is valid
+  if (element != null && date instanceof Date && !isNaN(date)) {
+    const date_options = {
+      weekday: "short",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const time_options = { 
+      hour: "2-digit", 
+      minute: "2-digit", 
+      hour12: false 
+    };
+    element.innerText = convertUTCDateToLocalDate(date, date_options, time_options);
+  }
+  else {
+    if (element == null) {
+      console.warn('An null element was passed to localDate, check the element exists on the current page.')
+    }
+    if (!(date instanceof Date) || isNaN(date)) {
+      console.warn('A non-date value was passed to localDate, check a valid datetime object is being passed.')
+    }
+  }
+}
+
+// Usage: document.getElementById("id").innerText = convertUTCDateToLocalDate(new Date('2021-08-12 09:58:22'));
+// Non-numeric month format will cause errors in multi-lingual setting
+let convertUTCDateToLocalDate = (date, date_options, time_options) => {
+  local_date = new Date(date.getTime() - date.getTimezoneOffset() * 60000);
+  return (
+    local_date.toLocaleDateString(undefined, date_options) +
+    " " +
+    local_date.toLocaleTimeString(undefined, time_options)
+  );
+};
+
+// set all external links to open in new tab
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('a[href^="http"], a[href^="/documents/"]').forEach(link => {
+    link.setAttribute('target', '_blank');
+    link.setAttribute('rel', 'nofollow noopener');
+  });
+});
+
+
+// change rich text <fa> font awesome tags: 
+// <fa style="display:none;">something</fa> -> <fa class="something">&nbsp;&nbsp;&nbsp;&nbsp;</fa>
+document.addEventListener('DOMContentLoaded', () => {
+  fa_icons = document.getElementsByTagName('fa');
+  for (let i = 0; i < fa_icons.length; i++) {
+    const fa_class = fa_icons[i].innerText;
+    if (fa_class) {
+      fa_icons[i].className = fa_icons[i].innerText;
+      fa_icons[i].innerHTML = "&nbsp;".repeat(4);
+      fa_icons[i].removeAttribute('style');
+    }
+  }
+});
+
+const include_js = (js, id) => {
+  return new Promise((resolve, reject) => {
+    let script_tag = document.getElementById(id);
+
+    if (!script_tag) {
+      const head = document.head || document.getElementsByTagName('head')[0];
+      script_tag = document.createElement('script');
+      script_tag.type = 'text/javascript';
+      script_tag.src = js;
+      script_tag.id = id;
+      script_tag.onload = resolve; // Resolve the promise when script is loaded
+      script_tag.onerror = reject; // Reject the promise on error
+      head.appendChild(script_tag);
+    } else {
+      resolve(); // Resolve the promise if script is already loaded
+    }
+  });
+};
+
+const highlightCodeBlock = (blockID, codeBlockCSS, theme, themeCSS, highlightScript, language, languageScript) => {
+  include_css(codeBlockCSS, "code-block-style");
+  include_css(themeCSS, `code-block-highlight-${theme}`);
+  Promise.all([
+    include_js(highlightScript, "code-block-highlight-script"),
+    include_js(languageScript, `code-highlight-${language}`)
+  ]).then(() => {
+    hljs.highlightElement(document.getElementById(blockID));
+  }).catch(error => {
+    console.error('Error loading scripts:', error);
+  });
+}
+
+const include_css = (css, id) => {
+  let link_tag = document.getElementById(id);
+
+  if (!link_tag) {
+    const head = document.head || document.getElementsByTagName('head')[0];
+    link_tag = document.createElement('link');
+    link_tag.rel = 'stylesheet';
+    link_tag.href = css;
+    link_tag.id = id;
+    head.appendChild(link_tag);
+  }
+
+  return link_tag;
+};
+
+
+const copyToClipboard = (event, id) => {
+  const buttonText = event.target.innerText
+  const copyText = document.getElementById(id);
+  navigator.clipboard.writeText(copyText.innerText);
+  event.target.innerText = 'Copied ✓';
+  event.target.classList.add('copied-to-clipboard');
+  setTimeout(() => {
+    event.target.innerText = buttonText;
+    event.target.classList.remove('copied-to-clipboard');
+  }, 1000);
+}
+
