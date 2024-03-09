9e3331c91db8c89a29b165417217c10904124d5c
Richard Allen <richard@enzedonline.com>
2024-03-10 09:03:44 +1300

    final

--- blocks/links.py
+++ blocks/links.py
@@ -7,48 +7,98 @@ from django.forms import RadioSelect
 from django.forms.utils import ErrorList
 from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
-from wagtail.blocks import (CharBlock, ChoiceBlock, PageChooserBlock,
-                            StructBlock, StructValue)
+from wagtail.blocks import (CharBlock, ChoiceBlock, StaticBlock, StructBlock,
+                            StructValue)
 from wagtail.blocks.struct_block import (StructBlockAdapter,
                                          StructBlockValidationError)
-from wagtail.documents.blocks import DocumentChooserBlock
 from wagtail.telepath import register
 
-from blocks.wagtail.blocks import URLBlock, RequiredMixin
+from blocks.data_blocks import (DataCharBlock, DataDocumentChooserBlock,
+                                DataExtendedURLBlock, DataPageChooserBlock, DataProductChooserBlock)
 
 
 class LinkTypeChoiceBlock(ChoiceBlock):
-    def __init__(self, *args, **kwargs):
-        super().__init__(widget=RadioSelect, *args, **kwargs)
-    
-    choices=[
-        ('page', _('Page Link')),
-        ('url', _('URL Link')),
-        ('document', _('Document Link')),
-    ]
+    """
+    Choice block of link types - 
+    link_types - the link types to offer as a choice, defaults ['page', 'url_link', 'document'], must be a subset of the default
+    unselected_label - the label to use on the no value choice in the case of required=False
+    default - the link_type value to set as default. If required and no default set, default will be first item in link_types
+    """
+    valid_link_types = ['page', 'url_link', 'document', 'product']
 
-class Link_Value(StructValue):
+    def __init__(self, link_types=valid_link_types, unselected_label=_("No Link"), required=True, default=None, *args, **kwargs):
+        if getattr(settings, 'DEBUG', False):
+            self.validate_link_types(link_types, default)
+        # Build choice list from link_types parameter
+        choices = [(key, value) for key, value in [
+            ('page', _('Page Link')),
+            ('url_link', _('URL Link')),
+            ('document', _('Document Link')),
+            ('product', _('Product Link')),
+        ] if key in link_types]
+        if required and not default:
+            # set default if required and no default supplied (no default will cause 'not selected' to be rendered)
+            default = choices[0][0]
+        elif not required:
+            # relabel 'not selected' button (default is '---------')
+            choices.insert(0, (None, unselected_label))
+        super().__init__(choices=choices, required=required,
+                         default=default, widget=RadioSelect(), *args, **kwargs)
+
+    def validate_link_types(self, link_types, default):
+        if not isinstance(link_types, list):
+            raise ImproperlyConfigured("link_types must be a list")
+        if not any(link_type in link_types for link_type in self.valid_link_types):
+            raise ImproperlyConfigured(
+                f"link_types must contain at least one of {', '.join(self.valid_link_types)}")
+        if any(link_type not in self.valid_link_types for link_type in link_types):
+            raise ImproperlyConfigured(
+                f"link_types must only contain the following elements: {', '.join(self.valid_link_types)}")
+        if default and default not in link_types:
+            raise ImproperlyConfigured(
+                f"Default value '{default}' not found in requested link types ({', '.join(link_types)})")
+
+
+class LinkValue(StructValue):
+    """
+    Return url and link text for all link types
+    """
+    i18n_enabled = getattr(settings, "WAGTAIL_I18N_ENABLED", False)
+
     def url(self) -> str:
         """Return a link's url regardless of link type"""
         try:
             match self.get("link_type"):
                 case 'page':
-                    internal_page = self.get("internal_page")
-                    i18n_enabled = getattr(settings, "WAGTAIL_I18N_ENABLED", False)
-                    url = internal_page.localized.url if i18n_enabled else internal_page.url
+                    internal_page = self.get("page")
+
+                    url = internal_page.localized.url if self.i18n_enabled else internal_page.url
                     return url + self.get("anchor_target")
-                case 'url':
+                case 'url_link':
                     # if needing to localise routable page relative urls, use template tag
                     # requires active site (get from request)
                     return self.get("url_link")
                 case 'document':
                     return self.get("document").url
+                case 'product':
+                    from product.models import Product, ProductPage
+                    base_page = ProductPage.objects.first()
+                    sku = self.get('product').sku
+                    product = Product.objects.filter(sku=sku).first()
+                    if product and base_page:
+                        if self.i18n_enabled:
+                            product = product.localized
+                            base_page = base_page.localized
+                        product_url_part = base_page.reverse_subpage(
+                            name='product_detail', kwargs={'sku': product.sku})
+                        return f"{base_page.url}{product_url_part}"
+                    return ''
         except Exception as e:
             logging.error(
                 f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}"
             )
         return None
-    
+
     def text(self) -> str:
         """Return link text - default to object title for page and document links"""
         link_text = self.get("link_text")
@@ -58,106 +108,171 @@ class Link_Value(StructValue):
             try:
                 match self.get("link_type"):
                     case 'page':
-                        internal_page = self.get("internal_page")
-                        i18n_enabled = getattr(settings, "WAGTAIL_I18N_ENABLED", False)
-                        return internal_page.localized.title if i18n_enabled else internal_page.title
+                        internal_page = self.get("page")
+                        return internal_page.localized.title if self.i18n_enabled else internal_page.title
+                    case 'url_link':
+                        return self.get("url_link")
                     case 'document':
                         return self.get("document").title
+                    case 'product':
+                        return self.get("product").title
             except Exception as e:
                 logging.error(
                     f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}"
                 )
-            return ''        
-    
-class LinkBlock(RequiredMixin, StructBlock):
+            return ''
 
+
+class LinkBlock(StructBlock):
     def __init__(
-            self, 
-            link_types=['page', 'url', 'document'], 
-            url_link_text_required=True,
-            no_link_label = _("No Link"),
-            no_link_description = _("No link selected."),
-            **kwargs
-        ):
-        super().__init__(**kwargs)
+        self,
+        required=True,
+        link_types=LinkTypeChoiceBlock.valid_link_types,
+        default_link_type=None,
+        url_link_text_required=True,
+        _unselected_label=_("No Link"),
+        _unselected_description=_("No link selected."),
+        **kwargs
+    ):
+        local_blocks = (
+            ('link_type', LinkTypeChoiceBlock(
+                required=required,
+                label=_("Link Type"),
+                link_types=link_types,
+                default=default_link_type,
+                unselected_label=_unselected_label
+            )),
+        )
+        if not required:
+            local_blocks += (
+                ('not_selected', StaticBlock(
+                    label=_unselected_description,
+                    admin_text="",
+                )),
+            )
+        if 'page' in link_types:
+            local_blocks += (
+                ('page', DataPageChooserBlock(
+                    required=False,
+                    label=_("Link to internal page"),
+                    attrs={'data-link-block-type': 'page'},
+                    chooser_attrs={'show_edit_link': False}
+                )),
+                ('anchor_target', DataCharBlock(
+                    required=False,
+                    label=_("Optional anchor target (#)"),
+                    attrs={'data-link-block-type': 'page'}
+                )),
+            )
+        if 'url_link' in link_types:
+            local_blocks += (
+                ('url_link', DataExtendedURLBlock(
+                    required=False,
+                    label=_("Link to external site or internal URL"),
+                    attrs={'data-link-block-type': 'url_link'}
+                )),
+            )
+        if 'document' in link_types:
+            local_blocks += (
+                ('document', DataDocumentChooserBlock(
+                    required=False,
+                    label=_("Link to document"),
+                    attrs={'data-link-block-type': 'document'},
+                    chooser_attrs={'show_edit_link': False}
+                )),
+            )
+        if 'product' in link_types:
+            local_blocks += (
+                ('product', DataProductChooserBlock(
+                    required=False,
+                    label=_("Link to product"),
+                    attrs={'data-link-block-type': 'product'},
+                    chooser_attrs={'show_edit_link': False}
+                )),
+            )
+        local_blocks += (
+            ('link_text', CharBlock(
+                required=False,
+                label=_("Link text"),
+            )),
+        )
+        super().__init__(local_blocks, **kwargs)
+        self._required = required
         self.link_types = link_types
-        self.no_link_label = no_link_label
         self.url_link_text_required = url_link_text_required
-        self.no_link_description = no_link_description
-        if getattr(settings, 'DEBUG', False): self.validate_link_types()
-        
-    link_type = LinkTypeChoiceBlock(required=False, label='')
-    internal_page = PageChooserBlock(required=False, label=_("Link to internal page"))
-    anchor_target = CharBlock(
-        required=False, 
-        label=_("Optional anchor target"), 
-        help_text=_("Anchor target must start with '#' followed by alphanumeric characters, hyphens and underscores.")
-    )
-    url_link = URLBlock(
-        required=False, label=_("Link to external site or internal URL")
-    )
-    document = DocumentChooserBlock(required=False, label=_("Link to document"))
-    link_text = CharBlock(required=False, label=_("Link text"))
+        self.no_link_label = _unselected_label
+        self.no_link_description = _unselected_description
 
     class Meta:
         template = 'blocks/link_block.html'
-        value_class = Link_Value
+        value_class = LinkValue
         icon = "link"
-        form_classname = "link-block"
+        form_classname = "struct-block link-block"
+        label_format = _("Link")
 
     def clean(self, value):
         errors = {}
-        link_type = value.get("link_type")
-        match link_type:
+        selected_link_type = value.get("link_type")
+        match selected_link_type:
             case 'page':
-                internal_page = value.get('internal_page')
+                internal_page = value.get('page')
                 anchor_target = value.get('anchor_target')
                 if not internal_page:
-                    errors['internal_page'] = ErrorList([_("Please select a page to link to")])
+                    errors['page'] = ErrorList(
+                        [_("Please select a page to link to")])
                 if anchor_target:
                     # add '#' if missing, validate format
                     if not anchor_target.startswith("#"):
                         anchor_target = f"#{anchor_target}"
                         value['anchor_target'] = anchor_target
                     if not re.match(r'^#[\w\-.]+$', anchor_target):
-                        errors['anchor_target'] = ErrorList([_("Please select a valid anchor target")])
-            case 'url':
+                        errors['anchor_target'] = ErrorList(
+                            [_("Anchor target must start with '#' followed by alphanumeric \
+                                characters, hyphens and underscores.")])
+            case 'url_link':
                 url_link = value.get('url_link')
                 if not url_link:
-                    errors['url_link'] = ErrorList([_("Please enter a URL to link to")])
+                    errors['url_link'] = ErrorList(
+                        [_("Please enter a URL")])
                 if self.url_link_text_required and not value.get('link_text'):
-                    errors['link_text'] = ErrorList([_("Please enter a display text for the link")])
+                    errors['link_text'] = ErrorList(
+                        [_("Please enter a display text for the link")])
             case 'document':
-                url_link = value.get('document')
-                if not url_link:
-                    errors['document'] = ErrorList([_("Please enter a URL to link to")])
+                document = value.get('document')
+                if not document:
+                    errors['document'] = ErrorList(
+                        [_("Please select a document to link to")])
+            case 'product':
+                document = value.get('product')
+                if not document:
+                    errors['product'] = ErrorList(
+                        [_("Please select a product to link to")])
             case _:
-                if self.required:
-                    errors['link_type'] = ErrorList([_("Please select a link type and value")])
+                if self._required:
+                    errors['link_type'] = ErrorList(
+                        [_("Please select a link type and value")])
         if errors:
             raise StructBlockValidationError(block_errors=errors)
 
+        # block validated - remove any link values for non-selected link types
+        for link_type in self.link_types:
+            if link_type != selected_link_type:
+                value[link_type] = None
+        if selected_link_type != 'page':
+            value['anchor_target'] = None
+
         return super().clean(value)
 
-    def validate_link_types(self):
-        if not isinstance(self.link_types, list):
-            raise ImproperlyConfigured("link_types must be a list")
-        if not any(link_type in self.link_types for link_type in ['page', 'url', 'document']):
-            raise ImproperlyConfigured("link_types must contain at least one of 'page', 'url', or 'document'")
-        if any(link_type not in ['page', 'url', 'document'] for link_type in self.link_types):
-            raise ImproperlyConfigured("link_types must only contain elements 'page', 'url', or 'document'")
 
 class LinkBlockAdapter(StructBlockAdapter):
-    js_constructor = "blocks.models.LinkBlock"
+    js_constructor = "blocks.links.LinkBlock"
 
     def js_args(self, block):
+        # keys added to args[2] found in this.meta in StructBlockDefinition
         args = super().js_args(block)
-        # link types to display as tabs
-        # passed to LinkBlock as parameter: link_types=['page', 'url', 'document']
+        # link types configured in LinkBlock class instance
         args[2]['link_types'] = block.link_types
-        # tab label and description for the empty value option
-        args[2]['no_link_label'] = block.no_link_label
-        args[2]['no_link_description'] = block.no_link_description
+        # add required '*' to link_text if url link selected and url_link_text_required==True
         args[2]['url_link_text_required'] = block.url_link_text_required
         return args
 
--- blocks/static/js/link-block.js
+++ blocks/static/js/link-block.js
@@ -1,14 +1,18 @@
-// js/link-this.linkBlock.js
+// js/linkBlock.js
 
 class LinkBlockDefinition extends window.wagtailStreamField.blocks
     .StructBlockDefinition {
     render(placeholder, prefix, initialState, initialError) {
+        // for each link type, mark the path field as required
         this.meta.link_types.forEach(link_type => {
             this.childBlockDefs.find(obj => obj.name === link_type).meta.required = true;
         })
+        // add required mark for link text if url_link_text_required===true
+        // required mark conditionally show if link_type==='url_link'
         if (this.meta.url_link_text_required){
             this.childBlockDefs.find(obj => obj.name === 'link_text').meta.required = true;
         }
+        // Wagtail block render
         const block = super.render(
             placeholder,
             prefix,
@@ -22,80 +26,73 @@ class LinkBlockDefinition extends window.wagtailStreamField.blocks
     };
 
     initialiseBlock(prefix) {
-        this.setupTabs(prefix);
-        this.setupLinkTypePanels();
-        // Link text section and required mark
-        this.linkBlock.linkTextSection = this.linkBlock.structBlock.querySelector('[data-contentpath="link_text"]');
-        this.linkBlock.linkTextRequiredMark = this.linkBlock.linkTextSection.querySelector("label.w-field__label>span.w-required-mark")
-        // Listen for tab click events
-        this.linkBlock.tabs.linkTypeSection.addEventListener('click', event => this.handleTabClick(event));
+        this.configureTabs(prefix);
+        this.configureLinkTypeChildBlocks();
         // Show panels for initially selected tab, or first tab by default
-        this.showPanels(this.linkBlock.tabs.buttons.find(button => button.checked) || this.linkBlock.tabs.buttons[0]);
+        this.showPanels(
+            this.linkBlock.tabs.buttons.find(button => button.checked) || this.linkBlock.tabs.buttons[0]
+        );
     }
 
-    setupTabs(prefix) {
-        // Cache link type wrapper and buttons
-        this.linkBlock.tabs = { buttons: [], panels: {} };
-        this.linkBlock.tabs.wrapper = this.linkBlock.structBlock.querySelector(`div#${prefix}-link_type`);
-        this.linkBlock.tabs.buttons = [...this.linkBlock.tabs.wrapper.querySelectorAll('input[type="radio"]')];
+    configureTabs(prefix) {
+        // the div container that wraps the radio button group
+        const tabList = this.linkBlock.structBlock.querySelector(`div#${prefix}-link_type`);
 
         // Modify link type styles - display as tabs
-        this.linkBlock.tabs.wrapper.role = "tablist";
-        this.linkBlock.tabs.wrapper.className = "w-tabs__list w-w-full link-block-tablist";
-        this.linkBlock.tabs.wrapper.querySelectorAll('div').forEach(div => {
+        tabList.role = "tablist";
+        tabList.className = "w-tabs__list w-w-full link-block-tablist";
+        // the div container that wraps each radio button
+        tabList.querySelectorAll('div').forEach(div => { 
             div.className = "link-block-tab";
         });
-        this.linkBlock.tabs.wrapper.querySelectorAll('label').forEach(label => {
+        // the radio button labels
+        tabList.querySelectorAll('label').forEach(label => { 
             label.className = "w-tabs__tab";
         });
 
-        // Hide redundant field label for 'link type' (but leave readable fo rcreen readers), move tabs row up
-        this.linkBlock.tabs.linkTypeSection = this.linkBlock.tabs.wrapper.closest('[data-contentpath="link_type"]');
-        this.linkBlock.tabs.linkTypeSection.querySelector("label.w-field__label").classList.add('visually-hidden');
+        // Cache link type radio input elements
+        this.linkBlock.tabs = { buttons: [...tabList.querySelectorAll('input[type="radio"]')] };
+
+        // Hide redundant field label for 'link type' (but leave readable for screen readers)
+        const linkTypeChildBlock = tabList.closest('[data-contentpath="link_type"]');
+        linkTypeChildBlock.querySelector("label.w-field__label").classList.add('visually-hidden');
+
+        // Listen for tab click events
+        linkTypeChildBlock.addEventListener('click', event => this.handleTabClick(event));
     }
 
-    setupLinkTypePanels() {
+    configureLinkTypeChildBlocks() {
         // associate child blocks with link type button
-        // child blocks should be declared with classname="link-block-tab-item link-type-XX" where XX is the link_type value
-        // these classes are added to the data-field element - the child block element is the div[data-contentpath] parent of that
-        // set data-parent-tab attribute on child block with value XX 
-        this.linkBlock.structBlock.querySelectorAll('div.link-block-tab-item').forEach(element => {
+        // child blocks should be declared with data-link-block-type='XX' where XX is the link_type value
+        // the child block element is the div[data-contentpath] parent of the element with data-link-block-type
+        // this 'XX' value is copied to the child-block container as data-parent-tab='XX'
+        // this attribute is used to show/hide child blocks on tab click
+        this.linkBlock.structBlock.querySelectorAll('[data-link-block-type]').forEach(element => {
             const childBlock = element.closest('div[data-contentpath]');
             if (childBlock) {
-                const linkType = Array.from(element.classList).find(
-                    classname => classname.includes("link-type-")
-                ).replace(/^link-type-/, '');
-                childBlock.setAttribute('data-parent-tab', linkType);
+                childBlock.setAttribute('data-parent-tab', element.dataset.linkBlockType);
             }
         });
-        // configure 'not selected' option if applicable
-        this.setupNotSelected();
-        // cache all tab item child blocks
-        this.tabItems = this.linkBlock.structBlock.querySelectorAll('div[data-parent-tab]');
-    }
-
-    setupNotSelected() {
-        // If link block required=False, relabel the 'not selected' button which has value===''
-        // 'Not selected' label is '---------' by default, replace text with this.meta.no_link_label 
-        // Associate not_selected StaticBlock with the 'not selected' button which has value===''
-        if (!this.meta.required) {
-            const noLinkSelectedButton = this.linkBlock.tabs.buttons.find(button => button.value === '');
-            // Radio button label text is a text node - replacing text on text node only preserves innerHTML (the <input> element) 
-            const emptyLabel = noLinkSelectedButton.closest('label');
-            emptyLabel.childNodes.forEach(node => {
-                if (node.nodeType === Node.TEXT_NODE) {
-                    node.textContent = this.meta.no_link_label;
-                    return false; // Exit the loop 
-                }
-            });
-            const noLinkSelectedText = this.linkBlock.structBlock.querySelector('div[data-contentpath="not_selected"]')
-            noLinkSelectedText.setAttribute('data-parent-tab', '');
+        // If optional link, associate not_selected StaticBlock with the 'not selected' tab 
+        // null choice radio button has value==='' so data-parent-tab='')
+        const noLinkSelectedText = this.linkBlock.structBlock.querySelector('div[data-contentpath="not_selected"]')
+        if (noLinkSelectedText) { 
+            noLinkSelectedText.setAttribute('data-parent-tab', ''); 
         }
+        // cache all tab item child blocks
+        this.linkBlock.tabs.tabItems = this.linkBlock.structBlock.querySelectorAll('div[data-parent-tab]');
+        // Cache link text child block and required mark
+        this.linkBlock.linkTextSection = this.linkBlock.structBlock.querySelector(
+            '[data-contentpath="link_text"]'
+        );
+        this.linkBlock.linkTextRequiredMark = this.linkBlock.linkTextSection.querySelector(
+            "label.w-field__label>span.w-required-mark"
+        )
     }
 
     showPanels(activeTab) {
         // When tab with value='XX' clicked, only those child blocks with data-parent-tab='XX' are visible
-        this.tabItems.forEach(div => {
+        this.linkBlock.tabs.tabItems.forEach(div => {
             div.style.display = (div.dataset.parentTab === activeTab.value) ? 'block' : 'none';
         });
         // set the active tab styling
@@ -105,7 +102,7 @@ class LinkBlockDefinition extends window.wagtailStreamField.blocks
         });
         // link text visible whenever active tab has a value (link type chosen)
         this.linkBlock.linkTextSection.style.display = (activeTab.value === '') ? "none" : "block";
-        // required mark for link tet only shown if url_link_text_required set in blockDef.meta and URL Link is active tab
+        // show required mark for link_text if url_link_text_required set in blockDef.meta and URL Link is active tab
         if (this.meta.url_link_text_required){
             this.linkBlock.linkTextRequiredMark.style.display = (
                 activeTab.value === 'url_link' && this.meta.url_link_text_required
@@ -115,12 +112,16 @@ class LinkBlockDefinition extends window.wagtailStreamField.blocks
 
     handleTabClick(event) {
         event.stopPropagation();
-        let clickedTab = event.target.closest('input[type="radio"]');
+        // find associated button - allows click on label and containing tab div
+        let clickedTab = event.target.closest('div.link-block-tab');
         if (clickedTab) {
-            this.showPanels(clickedTab);
+            let radioButton = clickedTab.querySelector('input[type="radio"]');
+            if (radioButton) {
+                this.showPanels(radioButton);
+            }
         }
     }
 
 }
 
-window.telepath.register('blocks.models.LinkBlock', LinkBlockDefinition);
+window.telepath.register('blocks.links.LinkBlock', LinkBlockDefinition);
