f70b46077031514898f3e7907b29d65f63bc1777
Richard Allen <richard@enzedonline.com>
2024-03-21 12:50:30 +1300

    code block collapsible

--- blocks/code.py
+++ blocks/code.py
@@ -1,5 +1,5 @@
 from django.utils.translation import gettext_lazy as _
-from wagtail.blocks import ChoiceBlock, StructBlock, TextBlock, StructValue
+from wagtail.blocks import ChoiceBlock, StructBlock, TextBlock, StructValue, CharBlock
 
 
 class CodeChoiceBlock(ChoiceBlock):
@@ -18,16 +18,25 @@ class CodeChoiceBlock(ChoiceBlock):
         ('powershell', 'PowerShell'),
     ]
 
+class CollapsibleChoiceBlock(ChoiceBlock):
+    choices=[
+        ('simple', 'Not Collapsible'),
+        ('collapsible', 'Collapsible'),
+        ('collapsed', 'Collapsed'),
+    ]    
+
 class CodeBlockValue(StructValue):   
     def theme(self) -> str:
         return "stackoverflow-dark"
 
 class BaseCodeBlock(StructBlock):
+    title = CharBlock(required=False)
+    type = CollapsibleChoiceBlock(required=True, default='simple')
     language = CodeChoiceBlock(default='python')
     code = TextBlock()
 
     class Meta:
-        template = "blocks/code_block.html"
+        template = "blocks/code-wrapper.html"
         icon = "code"
         label = _("Code Block")
         label_format = _("Code") + ": {language}"    
