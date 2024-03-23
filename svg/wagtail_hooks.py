790c3cb904d6c2dc8308247fdb407e02910e2aab
Richard Allen <richard@enzedonline.com>
2024-03-23 18:01:38 +1300

    stop universal styling

--- svg/wagtail_hooks.py
+++ svg/wagtail_hooks.py
@@ -12,20 +12,20 @@ def register_svg_image_chooser_viewset():
 
 register_snippet(SVGImage, viewset=SVGViewSet)
 
-@hooks.register('insert_global_admin_css')
-def svg_editor_css():
-    chooser_css = static('css/svg-chooser.css')
-    field_panel_css = static('css/svg-field-panel.css')
-    viewset_css = static('css/svg-viewset.css')
-    return mark_safe(
-        f'<link rel="stylesheet" href="{chooser_css}">\
-          <link rel="stylesheet" href="{field_panel_css}">\
-          <link rel="stylesheet" href="{viewset_css}">'
-    )
+# @hooks.register('insert_global_admin_css')
+# def svg_editor_css():
+#     chooser_css = static('css/svg-chooser.css')
+#     field_panel_css = static('css/svg-field-panel.css')
+#     viewset_css = static('css/svg-viewset.css')
+#     return mark_safe(
+#         f'<link rel="stylesheet" href="{chooser_css}">\
+#           <link rel="stylesheet" href="{field_panel_css}">\
+#           <link rel="stylesheet" href="{viewset_css}">'
+#     )
 
-@hooks.register('insert_global_admin_js')
-def svg_editor_js():
-    field_panel_js = static('js/svg-panel-preview.js')
-    return mark_safe(
-        f'<script src="{field_panel_js}"></script>'
-    )
+# @hooks.register('insert_global_admin_js')
+# def svg_editor_js():
+#     field_panel_js = static('js/svg-panel-preview.js')
+#     return mark_safe(
+#         f'<script src="{field_panel_js}"></script>'
+#     )
