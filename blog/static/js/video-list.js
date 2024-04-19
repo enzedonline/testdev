e5302d47b02bcfcb5b5f37d9db3f188c1b7abd6c
Richard Allen <richard@enzedonline.com>
2024-04-19 19:46:21 +1200

    +video page, video-list endless scroll

--- blog/static/js/video-list.js
+++ blog/static/js/video-list.js
@@ -1,106 +1,248 @@
-
-// load the youtube api
-const tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-document.head.appendChild(tag);
-
-const videoList = document.getElementById("video-list");
-const videoModal = document.getElementById("video-modal");
-const previousButton = document.getElementById("previous-video-button");
-const nextButton = document.getElementById("next-video-button");
-const youtubeVideos = document.querySelectorAll('div.youtube-card');
-let player; // holds the youtube player object
-
-// get all the video id's
-const youtubeVideoIds = [];
-youtubeVideos.forEach(video => {
-    youtubeVideoIds.push(video.dataset.videoId);
-});
-
-// find the adjacent entries in an array
-const findAdjacentIds = (id, idArray) => {
-    const index = idArray.indexOf(id);
-    let previousId = '';
-    let nextId = '';
-    if (index !== -1) {
-        if (index > 0) {
-            previousId = idArray[index - 1];
-        }
-        if (index < idArray.length - 1) {
-            nextId = idArray[index + 1];
+class VideoList {
+    constructor() {
+        window.test = this
+        this.videoListContainer = document.getElementById("video-list-container");
+        this.videoList = this.videoListContainer.querySelector("div#video-list");
+        this.videoCards = Array.from(this.videoListContainer.querySelectorAll("div.youtube-card"));
+        this.videoModal = this.videoListContainer.querySelector("div#video-modal");
+        this.previousVideoButton = this.videoListContainer.querySelector("div#video-modal-previous");
+        this.nextVideoButton = this.videoListContainer.querySelector("div#video-modal-next");
+        this.videoModalDismiss = this.videoListContainer.querySelector("button#video-modal-dismiss");
+        this.pagination = JSON.parse(this.videoListContainer.querySelector("script#pagination").textContent);
+        this.videoListContainer.dataset.cardsLoading = false;
+        this.videoLoaded = false;
+        this.loadYoutubeAPI();
+        this.addEventListeners();
+        if (this.pagination.enabled) {
+            this.loadCards();
         }
     }
-    return {
-        previous: previousId,
-        next: nextId
-    };
-}
-// load a video (autostart) and set prev/next video id's
-const playVideo = (videoID) => {
-    player.videoLoaded = false; // used to set position to 0sec
-    player.loadVideoById(videoID, startSeconds = 0);
-    const adjacents = findAdjacentIds(videoID, youtubeVideoIds);
-    previousButton.setAttribute("data-target", adjacents.previous);
-    nextButton.setAttribute("data-target", adjacents.next);
-}
 
-// close modal - stop video, hide modal
-const closeModal = () => {
-    player.stopVideo();
-    videoModal.classList.remove('show');
-}
-
-// event listeners
-videoModal.addEventListener("click", function (event) {
-    if (event.target === videoModal) {
-        closeModal();
-    }
-});
-previousButton.addEventListener("click", function (event) {
-    playVideo(previousButton.dataset.target);
-});
-nextButton.addEventListener("click", function (event) {
-    playVideo(nextButton.dataset.target);
-});
-
-// called when youtube api completes loading: creates the player iframe
-window.onYouTubeIframeAPIReady = () => {
-    player = new YT.Player('card-video-iframe', {
-        playerVars: {
-            'playsinline': 1
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+    loadYoutubeAPI() {
+        // load the youtube api
+        const tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/iframe_api";
+        document.head.appendChild(tag);
+        // called when youtube api completes loading: creates the player iframe
+        // onYouTubeIframeAPIReady must be created in window context
+        window.onYouTubeIframeAPIReady = () => {
+            window.player = new YT.Player('card-video-iframe', {
+                playerVars: {
+                    'playsinline': 1
+                },
+                events: {
+                    'onReady': this.onPlayerReady,
+                    'onStateChange': this.onPlayerStateChange
+                }
+            });
         }
-    });
-}
+    }
 
-// called when player iframe complates loading
-// - add video list click listener
-// - remove 'loading' class disable spinners and change cursor to pointer 
-const onPlayerReady = (event) => {
-    videoList.addEventListener("click", function (event) {
-        let targetElement = event.target;
-        while (targetElement !== null) {
-            if (targetElement.classList.contains("youtube-card")) {
-                playVideo(targetElement.dataset.videoId);
-                videoModal.classList.add('show');
-                return;
+    onPlayerReady = (event) => {
+        // called when player iframe completes loading - changes card behaviour
+        // via css: hides spinner, shows youtube logo, changes pointer to cursor
+        // via js: enables click
+        this.videoListContainer.dataset.playerLoading = false;
+    }
+
+    onPlayerStateChange = (event) => {
+        if (!this.videoLoaded) {
+            // bug fix: playVideoById() (and cueVideoById()) can ignore startSeconds attribute 
+            //   if user has part watched this video before, video will resume from where the user last left of instead
+            //   force video to start at 0 seconds by pausing video, using seekTo then restarting video
+            //   player muted during initial buffering to prevent audio fragment before video set to 0 seconds
+            //   use this.videoLoaded boolean flag to ensure only run once
+            if (event.data == YT.PlayerState.PLAYING) {
+                this.videoLoaded = true;
+                window.player.pauseVideo();
+                window.player.seekTo(0);
+                window.player.unMute()
+                window.player.playVideo();
             }
-            targetElement = targetElement.parentElement;
+            else if (event.data == YT.PlayerState.BUFFERING) {
+                window.player.mute();
+            }
+        } else if (event.data == YT.PlayerState.ENDED && this.nextVideoButton.dataset.target) {
+            // autoplay next video when finished if not last video
+            const nextVideo = this.nextVideoButton.dataset.target
+            this.playVideo(nextVideo);
         }
-    });
-    videoList.classList.remove('loading');
-}
-
-// fixes bug where api ignores start position and will play video from last user's last played position instead
-// set videoLoaded=true as player constantly changes state between PLAYING and BUFFERING
-const onPlayerStateChange = (event) => {
-    if (event.data == YT.PlayerState.PLAYING && !player.videoLoaded) {
-        player.pauseVideo();
-        player.seekTo(0);
-        player.playVideo();
-        player.videoLoaded = true;
     }
+
+    playVideo = (cardId) => {
+        // load a video (autostart) and set prev/next video id's
+        const videoId = this.videoList.querySelector(`div#${cardId}`).dataset.videoId
+        this.videoLoaded = false;
+        window.player.loadVideoById({ 'videoId': videoId, 'startSeconds': 0 });
+        this.setAdjacentIds(cardId);
+    }
+
+    async setAdjacentIds(cardId) {
+        // get id's for adjacent cards and load into prev/next buttons data-target
+        // use card id's, not video id's - video id's may not be unique
+        // the next button attribute used also for autoplaying next video on end
+        // preload additional videos if pagination enabled and near end of playlist
+        const videoCard = this.videoList.querySelector(`div#${cardId}`)
+        if (videoCard) {
+            let index = this.videoCards.indexOf(videoCard);
+            // previous/next are emtpy if first/last card (empty data-target buttons hidden via css)
+            let previous = index > 0 ? this.videoCards[index - 1].id : '';
+            let next = index < this.videoCards.length - 1 ? this.videoCards[index + 1].id : '';
+            this.previousVideoButton.setAttribute("data-target", previous);
+            this.nextVideoButton.setAttribute("data-target", next);
+            // preload additional videos if last or 2nd last video in playlist
+            if (this.pagination.enabled && (index > this.videoCards.length - 3)) {
+                await this.loadCards(false);
+                if (next === '') {
+                    index = this.videoCards.indexOf(videoCard);
+                    next = index < this.videoCards.length - 1 ? this.videoCards[index + 1].id : '';
+                    this.nextVideoButton.setAttribute("data-target", next);
+                }
+            }
+        }
+    }
+
+    async loadCards(auto=true) {
+        // if auto===true load, next set of videos only if bottom of videlist less than 150px below bottom of screen
+        // only run if not all pages have loaded
+        if (this.pagination.enabled) {
+            const containerRect = this.videoList.getBoundingClientRect();
+            const viewportHeight = window.innerHeight;
+            const containerBottom = containerRect.bottom;
+            if (!auto || containerBottom <= viewportHeight + 150) {
+                // fetch next page of video card data and update card set
+                try {
+                    // triggers fade in/out effect on thumbnail via css
+                    this.videoListContainer.dataset.cardsLoading = true;
+                    // fetch data for next page from api
+                    const url = `${window.location.href}api/page/${++this.pagination.current_page}/`;
+                    const response = await fetch(url);
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    const result = await response.json();
+                    this.videoData = result.videos;
+                    this.pagination = result.pagination;
+                    // create additional cards for loaded videos
+                    let loadedCards = ""
+                    for (let i = 0; i < this.videoData.length; i++) {
+                        loadedCards += this.cardHTMLFragment(
+                            this.videoData[i].card_id,
+                            this.videoData[i].video_id,
+                            this.videoData[i].thumbnail_url,
+                            this.videoData[i].title,
+                            this.videoData[i].author_name,
+                            this.videoData[i].description,
+                            this.localiseDate(this.videoData[i].last_updated)
+                        )
+                    }
+                    this.videoList.insertAdjacentHTML('beforeend', loadedCards);
+                    // load more cards if base of video list still within 150px of screen bottom
+                    await this.loadCards();
+                    // refresh playlist
+                    this.videoCards = Array.from(this.videoListContainer.querySelectorAll("div.youtube-card"));
+                    // end fade in/out effect on thumbnail via css - delay by 500ms
+                    setTimeout(() => {
+                        this.videoListContainer.dataset.cardsLoading = false;
+                        // final check if new card set needed - required if scrolled to end of page during loading
+                        window.dispatchEvent(new Event('scroll'));
+                    }, 500);
+                } catch (error) {
+                    console.error('There was a problem with the fetch operation:', error);
+                    this.videoListContainer.dataset.cardsLoading = false;
+                }
+            }
+        }
+    }
+
+    cardHTMLFragment(card_id, video_id, thumbnail_url, title, author_name, description, last_updated) {
+        // template for video card
+        return `
+        <div class="card youtube-card" id="${card_id}" data-video-id="${video_id}">
+            <div class="card-video">
+                <img class="card-video-thumbnail" src="${thumbnail_url}">
+                <svg class="youtube-logo">
+                    <use href="#icon-youtube-logo"></use>
+                </svg>
+            </div>
+            <div class="card-body">
+                <p class="h5 card-title">${title}</p>
+                <p class="h6 card-text card-author">${author_name}</p>
+                <div class="card-description">${description}</div>
+            </div>
+            <div class="card-footer">
+                <p class="h6 card-updated">
+                    Updated <span class="card-date">${last_updated}</span>
+                </p>
+            </div>
+        </div>
+        `
+    }
+
+    closeModal = () => {
+        // close modal - stop video, hide modal
+        window.player.stopVideo();
+        this.videoModal.classList.remove('show');
+    }
+
+    addEventListeners() {
+        // close modal if click anywhere on modal background
+        this.videoModal.addEventListener("click", (event) => {
+            if (event.target === this.videoModal) {
+                this.closeModal();
+            }
+        });
+        // close modal if click on close button
+        this.videoModalDismiss.addEventListener("click", (event) => {
+            this.closeModal();
+        });
+        // play previous video when click on modal left arrow
+        this.previousVideoButton.addEventListener("click", (event) => {
+            this.playVideo(this.previousVideoButton.dataset.target);
+        });
+        // play next video when click on modal right arrow
+        this.nextVideoButton.addEventListener("click", (event) => {
+            this.playVideo(this.nextVideoButton.dataset.target);
+        });
+        // open video when click anywhere on a video card
+        this.videoList.addEventListener("click", (event) => {
+            let targetElement = event.target;
+            while (targetElement !== null) {
+                if (targetElement.classList.contains("youtube-card")) {
+                    this.playVideo(targetElement.id);
+                    this.videoModal.classList.add('show');
+                    return;
+                }
+                targetElement = targetElement.parentElement;
+            }
+        });
+        // watch for scroll events if paginating
+        if (this.pagination.enabled) {
+            window.addEventListener('scroll', this.triggerEndlessScroll.bind(this), { passive: true });
+        }
+    }
+
+    triggerEndlessScroll() {
+        // pagination.enabled===true until last page loaded - ignore scroll event once fully loaded
+        // only trigger loadCards while cards not loading - ignore further scroll events until cards loaded
+        if (this.pagination.enabled && this.videoListContainer.dataset.cardsLoading !== 'true') {
+            this.loadCards();
+        }
+    }
+
+    localiseDate(dateTimeString) {
+        // display date in local format and adjust for local timezone
+        try {
+            const date = new Date(dateTimeString);
+            const options = {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            };
+            return date.toLocaleDateString(undefined, options);
+        } catch (error) {
+            console.error(`Could not convert ${dateTimeString} to a date: `, error);
+        }
+    }
+
 }
--- blog/video_page.py
+++ blog/video_page.py
@@ -1,8 +1,18 @@
+import logging
+import math
+import random
+import string
+from urllib.parse import parse_qs, urlparse
+
 from django.db import models
+from django.http import JsonResponse
+from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
 from modelcluster.fields import ParentalKey
 from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.contrib.routable_page.models import RoutablePageMixin, path
+from wagtail.embeds.embeds import get_embed
+from wagtail.embeds.exceptions import EmbedException
 from wagtail.fields import RichTextField
 from wagtail.models import Orderable, Page
 
@@ -20,10 +30,52 @@ class SimpleVideoOrderable(Orderable):
     class Meta(Orderable.Meta):
         verbose_name = "Video"
 
+    def __str__(self) -> str:
+        return self.url
+
+    @cached_property
+    def video_id(self):
+        """
+        Get youtube video ID from url
+        ID is last element of url path for youtu.be and shorts
+        """
+        id = None
+        parse = urlparse(self.url)
+        if parse.netloc.lower() == 'youtu.be' or parse.path.lower().startswith('/shorts'):
+            id = parse.path.split('/')[-1:]
+        elif parse.query:
+            id = parse_qs(parse.query).get('v')
+        return id[0] if id else ''
+    
+    @cached_property
+    def embed_attrs(self):
+        """
+        Get cached embed attributes from the db
+        """
+        try:
+            embed = get_embed(self.url)
+            return {
+                'video_id': self.video_id,
+                'thumbnail_url': embed.thumbnail_url,
+                'title': embed.title,
+                'author_name': embed.author_name,
+                'last_updated': embed.last_updated,
+            }
+        except EmbedException as e:
+            logging.warning(
+                f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: Unable to embed {self.url}"
+            )
+
 class VideoPage(RoutablePageMixin, Page):
+    """
+    Video index page with api for auto pagination via routable mixin
+    Page size - number of video cards to load per auto pagination (loaded via api)
+    Initial pages - number of card 'pages' for initial page load (loaded via context/template)
+    """
     parent_page_types = ['blog.BlogIndex']
     subpage_types = []
-    max_count = 1
+    page_size = 6
+    initial_pages = 2
 
     intro = RichTextField()
 
@@ -38,9 +90,77 @@ class VideoPage(RoutablePageMixin, Page):
     class Meta:
         verbose_name = _("Video Page")
 
+    def video_range(self, page=1, pages_to_load=1):
+        """
+        Return video card details for requested page.
+        Request multiple pages with pages_to_load.
+        Returns dictionary of videos and pagination status.
+        Number of cards per page set by page_size class attribute.
+        """
+        # ensure parameters are positive whole numbers       
+        try:
+            page = max(int(page), 1)
+        except (TypeError, ValueError):
+            page = 1
+        try:
+            pages_to_load = max(int(pages_to_load), 1)
+        except (TypeError, ValueError):
+            pages_to_load = 1
+
+        # caculate total pages including any final partial page
+        all_videos = self.videos.all()
+        total_pages = math.ceil(all_videos.count() / self.page_size)
+
+        # get video card set if requested page is <= total pages
+        videos = []
+        if page <= total_pages:
+            # caculate slice start & end
+            start = (page - 1) * self.page_size
+            if page + pages_to_load - 1 >= total_pages:
+                end = None # last page, get everything from slice start 
+            else:
+                end = start + (self.page_size * pages_to_load)
+            # create card set
+            for video in all_videos[start:end]:
+                details = video.embed_attrs
+                # only return card if embed valid
+                if details:
+                    # use random id for card to ensure each is unique on the page
+                    details['card_id'] = ''.join(random.choice(string.ascii_letters) for _ in range(8))
+                    details['description'] = video.description
+                    videos.append(details)
+                    
+        # disable pagination if last page reached (stops JS class auto-requesting new cards)
+        # current page calculated from requested page and pages_to_load
+        pagination = {
+            'enabled': page < total_pages,
+            'current_page': min((page - 1) + pages_to_load, total_pages),
+        }
+
+        return {'videos': videos, 'pagination': pagination}
+
+
     @path("")
-    def video_list(self, request, sort=None):
+    def video_index_page(self, request):
+        """ default page response """
+        # pre-load video cards on page load
+        # use initial_pages class attribute to pre-load multiple video card sets
+        # e.g. self.page_size = 6, self.initial_pages = 2 : pre-load 6x2=12 cards
+        video_range = self.video_range(page=1, pages_to_load=self.initial_pages)
         return self.render(
             request,
+            context_overrides={
+                "videos": video_range['videos'],
+                "pagination": video_range['pagination']
+            },
             template="blog/video-page.html",
         )
+    
+    @path("api/page/<int:page>/")
+    def paginated_video_json_response(self, request, page):
+        """ Endless scroll json response """
+        return JsonResponse(
+            self.video_range(page=page), 
+            safe=False
+        )
+    
