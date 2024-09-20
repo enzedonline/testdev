5d7da5d883647e157977e34fd924f996abe7e42f
rich <richard@enzedonline.com>
2024-09-21 11:59:17 +1200

    linting

--- core/viewsets/columns.py
+++ core/viewsets/columns.py
@@ -1,18 +1,22 @@
 from wagtail.admin.ui.tables import TitleColumn
 from django.utils.safestring import mark_safe
 
+
 class ImageColumn(TitleColumn):
     def get_cell_context_data(self, instance, parent_context):
         context = super().get_cell_context_data(instance, parent_context)
         try:
             context['value'] = mark_safe(
-                context['value'].get_rendition('height-50').img_tag({'class':"show-transparency"})
-                )
+                context['value'].get_rendition(
+                    'height-50').img_tag({'class': "show-transparency"})
+            )
         except:
             context['value'] = mark_safe(
-                '<svg class="icon icon-image" height="50px" viewBox="0 0 24 24" aria-hidden="true">'\
-                    '<use href="#icon-image"></use>'\
-                '</svg>'
-                )
+                '''
+                <svg class="icon icon-image" height="50px" 
+                    viewBox="0 0 24 24" aria-hidden="true">
+                    <use href="#icon-image"></use>
+                </svg>
+                '''
+            )
         return context
-
