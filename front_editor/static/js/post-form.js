0d3e6cea4fa2faca44a79d418a7d2906db420507
rich <richard@enzedonline.com>
2024-10-23 11:21:37 +1300

    2024-10-alpha

--- front_editor/static/js/post-form.js
+++ front_editor/static/js/post-form.js
@@ -3,24 +3,24 @@ const payloadModal = new bootstrap.Modal(document.getElementById('payloadModal')
 const maxPayloadSize = Number(form.dataset.maxPayload); // 1024 * 1024; // For example, 1 MB
 
 form.addEventListener('submit', (event) => {
-    // Prevent the form from submitting immediately
-    event.preventDefault();
+    if ((maxPayloadSize ?? 0) > 0) {
+        // estimate max overhead at 10kB
+        const estimatedOverhead = 10240
+        const payloadSize = (
+            new TextEncoder().encode(
+                new URLSearchParams(
+                    new FormData(form)
+                ).toString()
+            ).length + estimatedOverhead
+        ) / 1048576;
 
-    // Calculate the payload size
-    const formData = new FormData(form);
-    let payloadSize = 0;
-    for (const [key, value] of formData.entries()) {
-        // Calculate the size of each key-value pair
-        payloadSize += key.length + value.length;
+        // Check if the payload size exceeds the limit
+        if (payloadSize > maxPayloadSize) {
+            event.preventDefault();
+            form.querySelector('#payloadSize').textContent = payloadSize.toFixed(1);
+            form.querySelector('#maxPayloadSize').textContent = maxPayloadSize;
+            payloadModal.show();
+            return;
+        }
     }
-    payloadSize = payloadSize / (1024 * 1024);
-    // Check if the payload size exceeds the limit
-    if (payloadSize > maxPayloadSize) {
-        form.querySelector('#payloadSize').textContent = Math.ceil(payloadSize * 10) / 10;
-        form.querySelector('#maxPayloadSize').textContent = maxPayloadSize;
-        payloadModal.show();
-        return;
-    }
-
-    form.submit();
-});
+}, true);
