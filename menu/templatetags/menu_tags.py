495f6d7526ce85bb3a8d11306b964e3281386d9d
rich <richard@enzedonline.com>
2025-02-27 15:38:39 +1300

    refactor menu templates and tags for improved structure and functionality

--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -1,4 +1,8 @@
+import logging
+
 from django import template
+from django.conf import settings
+from django.db.models import Q
 from django.utils.safestring import mark_safe
 
 from core.utils import strip_svg_markup
@@ -6,31 +10,49 @@ from menu.models import Menu
 
 register = template.Library()
 
-@register.simple_tag()
-def load_menu(menu_slug):
-    try:
-        return Menu.objects.filter(slug=menu_slug).first().localized
-    except:
-        return Menu.objects.filter(slug=menu_slug).first()
+@register.inclusion_tag("menu/navbar.html", takes_context=True)
+def load_menu(context, menu_slug):
+    if getattr(settings, "WAGTAIL_I18N_ENABLED", False):
+        menu = Menu.objects.filter(slug=menu_slug).first().localized
+    else:
+        menu = Menu.objects.filter(slug=menu_slug).first()
+    return {
+        **context.flatten(),
+        'menu': menu,
+    }
    
-@register.filter()
-def show_on_menu(item, request):
+@register.simple_tag(takes_context=True)
+def show_on_menu(context, item):
+    """
+    Check if the item should be displayed on the menu based on the display_when option & user authenticated status
+    """
     try:
         display_when = item.get('options').get('display_when')
+        request = context.get('request')
         return (display_when in ['ALWAYS', None] or str(request.user.is_authenticated) == display_when)
-    except:
-        return True
+    except Exception as e:
+        logging.error(
+            f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}"
+        )
 
 @register.simple_tag(takes_context=True)
 def link_active(context, link):
+    """
+    If the link is the current page, return 'active' for the active class
+    """
     try:
-        return ' active' if link.url() == context['request'].path else ''
+        request = context.get('request')
+        return ' active' if link.url() == request.path else ''
     except:
         return ''
 
 @register.simple_tag()
 @mark_safe
-def menu_icon(image, redition_token='fill-25x25|format-png'):
+def menu_icon(image, rendition_token='fill-25x25|format-png'):
+    """
+    Return the image rendition tag for the menu icon
+    If image is svg, return as pure svg, stripped of script tags, height & width attributes
+    """
     if image:
         if image.is_svg():
             svg_file = image.file.file
@@ -39,12 +61,15 @@ def menu_icon(image, redition_token='fill-25x25|format-png'):
             svg_file.close()
             return strip_svg_markup(svg)
         else:
-            r = image.get_rendition(redition_token)
+            r = image.get_rendition(rendition_token)
             return r.img_tag()
     return ''
 
 @register.simple_tag(takes_context=True)
 def get_autofill_pages(context):
+    """
+    Return a list of pages to autofill the menu with
+    """
     autofill_block = context['self']
     links=[]
 
@@ -69,15 +94,15 @@ def get_autofill_pages(context):
         else:
             links.append(parent_page)
     
-    # return only public pages if user not authenticated
-    if authenticated:
-        children = parent_page.get_children().live().order_by(autofill_block['order_by'])
-    else:
-        children = parent_page.get_children().live().public().order_by(autofill_block['order_by'])
-
+    query = Q(live=True)
     # filter by 'Show in Menus' if selected
     if autofill_block['only_show_in_menus']:
-        children = children.filter(show_in_menus=True)
+        query &= Q(show_in_menus=True)
+    # return only public pages if user not authenticated
+    if authenticated:
+        children = parent_page.get_children().filter(query).order_by(autofill_block['order_by'])
+    else:
+        children = parent_page.get_children().public().filter(query).order_by(autofill_block['order_by'])
 
     for child in children[:autofill_block['max_items']]:
         if child.url == context['request'].path:
@@ -88,9 +113,16 @@ def get_autofill_pages(context):
 
 @register.simple_tag(takes_context=True)
 def render_user_info(context, msg):
-    user = context['request'].user
-    if user and "@username" in msg:
-        msg = msg.replace("@username", user.username)
-    if user and "@display_name" in msg:
-        msg = msg.replace("@display_name", getattr(user, 'display_name', user.get_full_name()))
-    return msg
+    """
+    Replace placeholders in the message with user info in the user menu
+    """
+    request = context.get('request')
+    user = request.user
+    if not user:
+        return None
+    else:
+        if "@username" in msg:
+            msg = msg.replace("@username", user.username)
+        if "@display_name" in msg:
+            msg = msg.replace("@display_name", getattr(user, 'display_name', user.get_full_name()))
+        return msg
