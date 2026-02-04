fb39e64d9b0b345051e70f535d5810581a39b36f
rich <richard@enzedonline.com>
2026-02-05 11:22:59 +1300

    f-string > format_html

--- product/wagtail_hooks.py
+++ product/wagtail_hooks.py
@@ -1,7 +1,7 @@
 # wagtail_hooks.py
 
 from django.templatetags.static import static
-from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 from wagtail import hooks
 
 from .views import product_chooser_viewset
@@ -11,14 +11,13 @@ from .views import product_chooser_viewset
 def register_product_chooser_viewset():
     return product_chooser_viewset
 
-@mark_safe
 @hooks.register('insert_global_admin_js')
 def register_admin_js():
     subcategory_chooser_js = static('js/subcategory_chooser.js')
-    return f'<script src="{subcategory_chooser_js}"></script>'
+    return format_html('<script src="{}"></script>', subcategory_chooser_js)
 
-@mark_safe
 @hooks.register('insert_global_admin_css')
 def register_admin_css():
     subcategory_chooser_css = static('css/subcategory_chooser.css')
-    return f'<link rel="stylesheet" href="{subcategory_chooser_css}">'
+    return format_html('<link rel="stylesheet" href="{}">', subcategory_chooser_css)
+
