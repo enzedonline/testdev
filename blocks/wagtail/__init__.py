05f0e0780098f0584423a0172b2f16845eda9f78
richard@enzedonline.com <richard@enzedonline.com>
2026-03-19 10:57:55 +1300

    Add imports for block components in wagtail module

---
+++ blocks/wagtail/__init__.py
@@ -0,0 +1,6 @@
+from ...blocks import ImageChooserBlock
+from ...blocks import PageChooserBlock
+from ...blocks import CharBlock
+from ...blocks import ChoiceBlock
+from ...blocks import TextBlock
+from ...blocks import URLBlock
